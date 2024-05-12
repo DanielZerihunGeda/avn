d76c63b4952bdd74af154afc83419c45c33b6bf1
Daniel Zerihun Geda <152178755+DanielZerihunGeda@users.noreply.github.com>
2024-05-12 08:39:56 +0300

    regular expression is added

--- map.py
+++ map.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import altair as alt
+import re
 def convert_google_sheet_url(url):
     # Regular expression to match and capture the necessary part of the URL
     pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
