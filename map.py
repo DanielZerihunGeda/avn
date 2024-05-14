f1501bfce2b339c1e19286c00d5efffe02e185f1
Tihetna <102287467+Tihetna1891@users.noreply.github.com>
2024-05-14 12:26:36 +0300

    Update map.py

--- map.py
+++ map.py
@@ -27,18 +27,17 @@ def visualize_min_price_per_each_category(df, selected_date_range, selected_cate
     df['timestamp'] = pd.to_datetime(df['timestamp']) 
     # Filter DataFrame for the selected date range and product 
     
-    category_location_map = {
-        "Suk": "Specific location for Suk",
-        "Super Market": "Specific location for Super Market",
-        "Sunday Market": "Specific location for Sunday Market",
-    }
+    selected_categories = df["Location"].isin(bench_mark)
+        for category in selected_categories:
+            if category == "Suk":
+                data.loc[data["Category"] == category, "Location"] = full_data[full_data["Category"] == category]["Location"] 
 
    
 
     filtered_data = df[ 
         (df['timestamp'] >= start_date) &  
         (df['timestamp'] <= end_date) &  
-        (df["Category"].isin(bench_mark))&
+       
     ]
         
 
