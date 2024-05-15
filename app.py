d5ddddcd1eea7e53efcae3dc8af61cb9319d9865
DanielZerihunGeda <lenjisa48@gmail.com>
2024-05-15 04:55:01 +0300

    select improved

--- app.py
+++ app.py
@@ -4,62 +4,63 @@ import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
 from map import *
+
 st.title("DASHBOARD")
 st.sidebar.title("SIDE BAR")
 st.markdown("Select filters to visualize the dashboard")
-st.sidebar.markdown("This application is a Streamlit app used to Track pricing survey")
+st.sidebar.markdown("This application is a Streamlit app used to track pricing survey")
 
-# Convert Google Sheets URL to CSV URL
+# Load data
 url = convert_google_sheet_url('https://docs.google.com/spreadsheets/d/1QJi3WJoBHQ9X92ezeTE8WHZM3gCQMJweOXmLmcw-phA/edit?usp=sharing')
-
-# Load data from CSV
 survey = pd.read_csv(url)
 
-# Date range selection
+# Convert 'timestamp' column to datetime (if not already done)
+survey['timestamp'] = pd.to_datetime(survey['timestamp'])
+
+# Sidebar date picker for selecting date range
 selected_date_range = st.sidebar.date_input("Select Date Range", 
                                            value=(pd.to_datetime('today') - pd.to_timedelta(7, unit='d'), 
                                                   pd.to_datetime('today')), 
                                            key="date_range")
-start_date = selected_date_range[0]
-end_date = selected_date_range[1]
-if start_date > end_date:
-  st.sidebar.error("Please select a valid date range.")
-bench_mark = st.sidebar.multiselect("Minmum Price",
-                                    ('Suk',
-                                    'Super Market',
-                                    'Sunday Market'),
-                                            key='unique_key_3')
 
-selected_product = st.sidebar.selectbox("Select Product", 
-                                        ('Red Onion Grade A  Restaurant q', 'Red Onion Grade B',
-                                         ' Red Onion Grade C', 'Potatoes', 'Potatoes Restaurant quality',
-                                         'Tomatoes Grade B', 'Tomatoes Grade A', 'Carrot', 'Chilly Green',
-                                         ' Chilly Green (Elfora)', 'Beet root', 'White Cabbage', 'Avocado',
-                                         'Strawberry', 'Papaya', 'Courgette', 'Cucumber', 'Garlic',
-                                         'Ginger', 'Pineapple', 'Mango', 'Lemon', 'Apple',
-                                         'Valencia Orange', 'Yerer Orange', 'Avocado Shekaraw'),
-                                        key='unique_key_2')
+# Filter data based on the selected date range
+filtered_survey = survey[(survey['timestamp'] >= pd.Timestamp(selected_date_range[0])) & 
+                         (survey['timestamp'] <= pd.Timestamp(selected_date_range[1]))]
 
-# Benchmark location selection
-selected_locations = st.sidebar.multiselect("Pick Location", 
-                                            ('benchmark location 1 Sunday market Piaza',
-                                             'benchmark location 2 Sunday market Bole',
-                                             'benchmark location 3 Sunday market Gerji',
-                                             'benchmark location 1 Suk Bole', 'benchmark location 2 Suk Gulele',
-                                             'benchmark location 3 Suk Yeka', 'benchmark location 4 Suk Arada',
-                                             'benchmark locatio_by_location 5 Suk Lideta',
-                                             'benchmark location 1 supermarket Queens',
-                                             'benchmark location 2 supermarket Purpose black',
-                                             'benchmark location 1 supermarket Queens - per 5 pieces',
-                                             'benchmark location 4 Sunday market Kolfe',
-                                             'benchmark location 6 Suk Kolfe',
-                                             'benchmark location 3 supermarket Freshcorner',
-                                             'Distribution center 1 Gerji', 'Distribution center 2 Garment',
-                                             'Distribution center 3 02 ',
-                                             'Distribution center Lemi kura/ Alem bank',
-                                             'Distribution center 1 Gerji (Raw)',
-                                             'Distribution center 2 Garment (Raw)',
-                                             'Distribution center Lemi kura',
-                                            'Cost per kg farm'), 
-                                            key='unique_key_1')
-visualize_price_by_location(survey, selected_date_range, selected_product, selected_locations)
+# Extract unique products and locations
+available_products = filtered_survey['Products List'].unique()
+available_locations = filtered_survey['Location'].unique()
+
+# Sidebar select boxes
+selected_product = st.sidebar.selectbox("Select Product", available_products, key='unique_key_2')
+# Define groups of locations
+location_groups = {
+    "Local Shops": ['benchmark location 1 Suk Bole', 'benchmark location 2 Suk Gulele',
+       'benchmark location 3 Suk Yeka', 'benchmark location 4 Suk Arada',
+       'benchmark location 5 Suk Lideta','benchmark location 6 Suk Kolfe',],
+    "Supermarkets": [ 'benchmark location 1 supermarket Queens',
+       'benchmark location 2 supermarket Purpose black',
+       'benchmark location 1 supermarket Queens - per 5 pieces',],
+    "Sunday Markets":['benchmark location 1 Sunday market Piaza','benchmark location 2 Sunday market Bole',
+       'benchmark location 3 Sunday market Gerji'],
+    "Distribution Centers":['Distribution center 1 Gerji', 'Distribution center 2 Garment',
+       'Distribution center 3 02',
+       'Distribution center Lemi kura/ Alem bank',
+       'Distribution center 1 Gerji (Raw)',
+       'Distribution center 2 Garment (Raw)',
+       'Distribution center Lemi kura'],
+     "Farm":[]  
+}
+
+all_locations = []
+
+for group, locations in location_groups.items():
+    selected_locations = st.sidebar.multiselect(f"Pick Location ({group})", locations)
+    all_locations.extend(selected_locations)
+
+# Filter data based on selected locations
+filtered_survey = survey[survey['Location'].isin(all_locations)]
+
+
+# Visualize data
+visualize_price_by_location(filtered_survey, selected_date_range, selected_product, selected_locations)
--- map.py
+++ map.py
@@ -6,51 +6,14 @@ import re
 def convert_google_sheet_url(url):
     # Regular expression to match and capture the necessary part of the URL
     pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
-
-    # Replace function to construct the new URL for CSV export
-    # If gid is present in the URL, it includes it in the export URL, otherwise, it's omitted
     replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'
-
-    # Replace using regex
     new_url = re.sub(pattern, replacement, url)
-
     return new_url
-def visualize_min_price_per_each_category(df, selected_date_range, selected_categories):
-    start_date, end_date = selected_date_range 
-    suk, supermarket, sundaymarket = selected_categories
-
-    # Convert start_date and end_date to datetime objects 
-    start_date = pd.to_datetime(start_date) 
-    end_date = pd.to_datetime(end_date)
-
-    # Convert 'timestamp' column to datetime format 
-    df['timestamp'] = pd.to_datetime(df['timestamp']) 
-    # Filter DataFrame for the selected date range and product 
-    mapping_location_to_categories= {
-        
-    }
-    filtered_data = df[ 
-        (df['timestamp'] >= start_date) &  
-        (df['timestamp'] <= end_date) &  
-        
-
-    
-
-    
-
-
-
 def visualize_price_by_location(df, selected_date_range, selected_product, selected_locations): 
     start_date, end_date = selected_date_range 
-
-    # Convert start_date and end_date to datetime objects 
     start_date = pd.to_datetime(start_date) 
     end_date = pd.to_datetime(end_date) 
-
-    # Convert 'timestamp' column to datetime format 
-    df['timestamp'] = pd.to_datetime(df['timestamp']) 
-
-    # Filter DataFrame for the selected date range and product 
+    df['timestamp'] = pd.to_datetime(df['timestamp'])  
     filtered_data = df[ 
         (df['timestamp'] >= start_date) &  
         (df['timestamp'] <= end_date) &  
@@ -71,10 +34,10 @@ def visualize_price_by_location(df, selected_date_range, selected_product, selec
 
     # Create Altair chart with side legend for full string display
     chart = alt.Chart(filtered_data).mark_line(interpolate='basis').encode( 
-        x=alt.X('timestamp:T', axis=alt.Axis(title='Date')), 
+        x=alt.X('yearmonthdate(timestamp):T', axis=alt.Axis(title='Date')), 
         y=alt.Y('average_price:Q', title='Average Price', scale=alt.Scale(zero=False)), 
         color=alt.Color('Location:N', legend=alt.Legend(title='Location', orient='right', labelLimit=400)), 
-        tooltip=['timestamp:T', 'Location:N', 'average_price:Q'] 
+        tooltip=['yearmonthdate(timestamp):T', 'Location:N', 'average_price:Q'] 
     ).properties( 
         width=600, 
         height=400, 
