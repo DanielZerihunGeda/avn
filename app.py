f2de99da0ec5339976b0c298d6928b62174d399e
Daniel Zerihun Geda <152178755+DanielZerihunGeda@users.noreply.github.com>
2024-05-23 05:21:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ import matplotlib.pyplot as plt
 import re
 import requests
 from io import StringIO
-from map import *
+from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
+import json
 from dotenv import load_dotenv
 import os
+from st_aggrid import AgGrid
+from map import *
 
-#inittializing environment
-load_dotenv()
+
 def clean_location_name(location, filtered_survey):
     cleaned_name = re.sub(r'benchmark location \d+', '', location)
     cleaned_name = re.sub(r'Distribution center \d+', '', cleaned_name)
@@ -28,57 +32,63 @@ def fetch_google_sheet_csv(url):
     except requests.exceptions.RequestException as e:
         st.error(f"Error fetching data: {e}")
         return None
+
+st.set_page_config(layout="wide")
+
 st.title("DASHBOARD")
 st.sidebar.title("SIDE BAR")
 st.markdown("Select filters to visualize the dashboard")
 st.sidebar.markdown("This application is a Streamlit app used to track pricing survey")
-col = st.columns((1.5, 4.5, 2), gap='medium')
-# Convert Google Sheets URL to CSV URL
+
 url_1 = convert_google_sheet_url(st.secrets["url_1"])
 url = convert_google_sheet_url(st.secrets["url"])
 url_2 = convert_google_sheet_url(st.secrets["url_2"])
-st.write(f"Converted URL: {url},{url_1},{url_2}") 
+url_3 = convert_google_sheet_url(st.secrets["url_3"])
+#st.write(f"Converted URL: {url},{url_1},{url_2},{url_3}") 
 
 try:
     survey_0 = pd.read_csv(url)
     survey_1 = pd.read_csv(url_1)
     survey_2 = pd.read_csv(url_2)
+    survey_3 = pd.read_csv(url_3)
     survey_2 = survey_2.rename(columns={'Buying Price': 'Unit Price','Location ': 'Location', 'Product List': 'Products List'})
-    st.write("Data loaded successfully!")  # Debugging statement
+    survey_3 = survey_3.rename(columns={'Buying Price per Kg ': 'Unit Price','Product Origin ':'Location','Product List':'Products List'})
+    st.write("Data loaded successfully!") 
 except Exception as e:
     st.error(f"Failed to load data into DataFrame: {e}")
     st.stop()
 
-# Convert timestamp to date only
 survey_0['Timestamp'] = pd.to_datetime(survey_0['Timestamp'], format= "%m/%d/%Y %H:%M:%S").dt.date
 survey_1['Timestamp'] = pd.to_datetime(survey_1['Timestamp'], format="%Y-%m-%d %H:%M:%S").dt.date
 survey_2['Timestamp'] = pd.to_datetime(survey_2['Timestamp'], format= "%m/%d/%Y %H:%M:%S").dt.date
+survey_3['Timestamp'] = pd.to_datetime(survey_3['Timestamp'], format="%Y-%m-%d %H:%M:%S").dt.date
+survey = concatenate_dfs(survey_0, survey_1, survey_2, survey_3)
 
-survey = concatenate_dfs(survey_0,survey_1,survey_2)
-
-selected_date_range = st.sidebar.date_input("Select Date Range", 
-                                            value=(pd.to_datetime('today') - pd.to_timedelta(7, unit='d'), 
-                                                   pd.to_datetime('today')), 
-                                            key="date_range")
-
-# Convert selected_date_range to dates only
+default_start = pd.to_datetime('today') - pd.to_timedelta(7, unit='d')
+default_end = pd.to_datetime('today')
+start_date = st.sidebar.date_input(
+    "From", 
+    value=default_start, 
+    key="start_date"
+)
+end_date = st.sidebar.date_input(
+    "To", 
+    value=default_end, 
+    key="end_date"
+)
+selected_date_range = (start_date, end_date)
 start_date = selected_date_range[0]
 end_date = selected_date_range[1]
 
 filtered_survey = survey[(survey['Timestamp'] >= start_date) & 
                          (survey['Timestamp'] <= end_date)]
-
 available_products = filtered_survey['Products List'].unique()
 available_locations = filtered_survey['Location'].unique()
 
 selected_product = st.sidebar.selectbox("Select Product", available_products, key='unique_key_2')
-# price_metric = st.sidebar.radio("Select Price Metric", ['Minimum', 'Average'], key='price_metric')
-
 end_date_data = survey[(survey['Products List'] == selected_product) & 
                        (survey['Timestamp'] == end_date)]
 
-
-min_price_locations = {}
 location_groups = {
     "Local Shops": ['benchmark location 1 Suk Bole', 'benchmark location 2 Suk Gulele',
                     'benchmark location 3 Suk Yeka', 'benchmark location 4 Suk Arada',
@@ -92,27 +102,47 @@ location_groups = {
                              'Distribution center 3 02', 'Distribution center Lemi kura/ Alem bank',
                              'Distribution center 1 Gerji (Raw)', 'Distribution center 2 Garment (Raw)',
                              'Distribution center Lemi kura'],
-    "Farm": []
+    "Farm":['Mekele', 'Kobo', 'Dansha']
 }
+
 col = st.columns((5.5, 3), gap='medium')
-with st.sidebar.expander("Show Analysis", expanded=False):
-    for group, locations in location_groups.items():
-        group_data = end_date_data[end_date_data['Location'].isin(locations)]
-        if not group_data.empty:
-            min_price = group_data['Unit Price'].min()
-            avg_price = group_data['Unit Price'].mean()
-            min_location = group_data.loc[group_data['Unit Price'].idxmin()]['Location']
-            st.sidebar.markdown(f"**{group}** - Min: {min_price}, Avg: {avg_price}")
-        else:
-            min_location = None
-            st.sidebar.markdown(f"**{group}** - No data available")
-        min_price_locations[group] = min_location
 
 cleaned_location_groups_with_counts = {group: [clean_location_name(loc, filtered_survey) for loc in locations] for group, locations in location_groups.items()}
 reverse_location_mapping = {clean_location_name(loc, filtered_survey): loc for loc in survey['Location'].unique()}
-
 all_sorted_locations = []
+selected_groups = st.sidebar.multiselect(
+        "Select Location Groups",
+        options=list(location_groups.keys()),
+        default=list(location_groups.keys())
+    )
 key_counter = 0
+with st.sidebar.form(key='price_form'):
+    individual_price = st.number_input("Set Individual Price:", min_value=0.0, format="%.2f")
+    group_price = st.number_input("Set Group Price:", min_value=0.0, format="%.2f")
+    bulk_price = st.number_input("Set Bulk Price:", min_value=0.0, format="%.2f")
+    submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        if individual_price == 0 and group_price == 0 and bulk_price == 0:
+            st.warning("Please set at least one price.")
+        else:
+            # Create a DataFrame with the submitted data
+            current_time = datetime.now()
+            data = {
+                'Individual Price': [individual_price],
+                'Group Price': [group_price],
+                'Bulk Price': [bulk_price],
+                'Timestamp': [current_time.strftime("%Y-%m-%d %H:%M:%S")]
+            }
+            df = pd.DataFrame(data)
+            
+            # Path to your credentials file
+                  # Replace with the path to your credentials file
+            
+                        # Append the data to the Google Sheet
+            append_df_to_gsheet('bekele', df)
+            
+            st.success('Data submitted successfully!')
 for group, sorted_locations in cleaned_location_groups_with_counts.items():
     is_expanded = key_counter == 0
     with st.sidebar.expander(f"Pick Location ({group})", expanded=is_expanded):
@@ -123,30 +153,19 @@ for group, sorted_locations in cleaned_location_groups_with_counts.items():
     key_counter += 1
 
 filtered_survey = survey[survey['Location'].isin(all_sorted_locations)]
-individual_price = st.sidebar.number_input("set Individual price: ")
-individual_price = st.sidebar.number_input("set Group Price: ")
-bulk_price= st.sidebar.number_input("set Bulk Price: ")
 
-with col[0]:
-    visualize_price_by_location(filtered_survey, selected_date_range, selected_product, all_sorted_locations)
-with col[1]:
-    df = calculate_min_prices(survey, selected_date_range, location_groups)
-    st.markdown(f"### Minimum and Average Price in {end_date.strftime('%Y-%m-%d')}")
+visualize_price_by_location(filtered_survey, selected_date_range, selected_product, all_sorted_locations)
 
-    st.dataframe(df,
-                 column_order=("Min Location","Products List", "Min Price", "Avg Price"),
-                 hide_index=True,
-                 width=None,
-                 column_config={
-                    "Min Location": st.column_config.TextColumn(
-                        "Location", max_chars=5,default="st.",width='small'
-                    ),
-                    "Products List": st.column_config.TextColumn(
-                        "Prodcut", max_chars=5,width='small'
-                    ),
-                    "Min Price": st.column_config.NumberColumn(
-                        "Min Price",
-                     ),"Avg Price": st.column_config.NumberColumn(
-                        "Avg Price",
-                     )}
-                 )
+df = calculate_min_prices(survey, selected_date_range, selected_product, location_groups)
+df1 = calculate_prices_by_location(survey, selected_date_range, selected_product, location_groups)
+st.write(df["Local Shops"])
+collapsible_table("Local Shops",df1["Local Shops"])
+st.write(df["Supermarkets"])
+collapsible_table("Supermarkets",df1["Supermarkets"])
+st.write(df["Sunday Markets"])
+collapsible_table("Sunday Markets",df1["Sunday Markets"])
+st.write(df["Farm"])
+collapsible_table("Farm", df1["Farm"])
+plot_min_price_trends(survey, selected_date_range, selected_product, location_groups, selected_groups)
+
+
